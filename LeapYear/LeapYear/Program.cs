7f3f96d7b6cd5c128139afb8acd32e5b490e7ea2
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-09-11 19:08:56 +0100

    program to calculate Leap Year

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+
+
+class Program {
+  // Print Leap year
+
+static void FindLeapYear(){
+  int stYear, enYear;
+  Console.WriteLine("Enter Starting Year");
+  stYear = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Enter Ending Year");
+  enYear = Convert.ToInt32(Console.ReadLine());
+
+  for(int n = stYear; n <= enYear; n++){
+      if((n % 4) == 0){
+        Console.WriteLine("Leap Year {0}", n);
+      }
+  }
+  
+}
+  public static void Main (string[] args) {
+    FindLeapYear();
+  }
+}
