422b7f34070c44b28b228bbfd64db9a45de6cc40
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-09-14 07:40:50 +0100

    Text-Translation-App

---
+++ text_Translate/text_translator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Pig LatinTranslator");
+        Console.WriteLine();
+
+        Console.Write("Enter Word:  ");
+        string word = Console.ReadLine();
+
+        //Step One
+        int vowelPosition = -1;
+        foreach (char letter in word)
+        {
+            vowelPosition = vowelPosition + 1;
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+            {
+                break;
+            }
+        }
+        //Step Two
+        string pigLatinWord = "";
+        string beforeLetters = "";
+        string afterLetters = "";
+
+        switch (vowelPosition)
+        {
+            case 0:
+                pigLatinWord = word + "way";
+                break;
+            case 1:
+                beforeLetters = word.Substring(0, 1);
+                afterLetters = word.Substring(1);
+                pigLatinWord = afterLetters + beforeLetters + "ay";
+                break;
+            case 2:
+                beforeLetters = word.Substring(0, 2);
+                afterLetters = word.Substring(2);
+                pigLatinWord = afterLetters + beforeLetters + "ay";
+                break;
+            default:
+                pigLatinWord = "unable to translate this word, am Sorry!";
+                break;
+        }
+        Console.WriteLine("Translation: " + pigLatinWord);
+        Console.WriteLine();
+        Console.WriteLine("-----Press any key to exit this buffer");
+        Console.ReadKey();
+    }
+}
